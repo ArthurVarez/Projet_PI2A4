916d544f1a93864232ad85cc7473f0b3ac6b2dd8
ArthurVarez <arthur.varez@gmail.com>
2021-01-13 16:04:50 +0100

    docker-compose

--- app/app.py
+++ app/app.py
@@ -1,10 +1,10 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import create_engine
+#from flask_sqlalchemy import SQLAlchemy
+#from sqlalchemy import create_engine
 
 app = Flask(__name__)
-engine = create_engine('mysql+mysqldb://user:password@0.0.0.0/adminer')    
-connnect = engine.connect()
+#engine = create_engine('mysql+mysqldb://user:password@0.0.0.0/adminer')    
+#connnect = engine.connect()
 
 
 
@@ -13,8 +13,8 @@ connnect = engine.connect()
 def hello():
     return '<h1> Hello <h1>'
 
-@appr.route()
+
 
 
 if __name__ == '__main__':
-    app.run(host=0.0.0.0,port=5000, debug=True)
+    app.run(port=5000, debug=True)
