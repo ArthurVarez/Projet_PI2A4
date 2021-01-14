3b77f0834b22c7cd6e604059f8542adc1de6dee3
remid1 <remi.delattre@edu.devinci.fr>
2021-01-14 16:56:13 +0100

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,4 +1,6 @@
 from flask import Flask
+from flask import request
+
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import create_engine, Column, Integer,String,DateTime, Table , MetaData
 from sqlalchemy.orm import sessionmaker,scoped_session
@@ -8,7 +10,7 @@ from sqlalchemy.sql import select
 app = Flask(__name__)
 
 engine = create_engine('mysql+pymysql://root:password@localhost:3306/API_DB')
-Session = sessionmaker(bind=engine,future=True)
+Session = sessionmaker(bind=engine)
 Base= declarative_base()
 metadata= MetaData()
 """
@@ -48,35 +50,49 @@ ressources = Table(
 )
 
 
-
-
-@app.route("/")
-
-def hello():
-    return '<h1> Hello <h1>'
-
-@app.route("/Ressource",methods=["GET"])
-
-def get_ressources():
-    #ressources = Ressource()
-    query = select([ressources])
-    conn = engine.connect()
-    res = conn.execute(query)
-    chain = ""
-    for element in res:
-        chain = chaine + str(element)
-    res.close()
-    return chain
-
-"""@app.route("/Reservation",methods=["GET"])
-    def get_reservation():
-        reservations = Reservation()
+@app.route("/Ressource/",methods=["GET","POST"])
+def Ressources():
+    if request.method=="GET": #Renvoie la liste des Ressources
+        #ressources = Ressource()
+        query = select([ressources])
+        conn = engine.connect()
+        res = conn.execute(query)
+        chain = ""
+        for element in res:
+            chain = chain + str(element)
+        res.close()
+        return chain
+    elif request.method=="POST": #permet d'ajouter une Ressources(il faudra verifier que l'utilisateur est admin)
         pass
 
-@app.route("/RessourceReserve")
-    def get_ressourcereserve():
+@app.route("/Ressource/<id>/", methods=["GET"])
+def Ressource_ID(): #renvoie la ressource avec l'ID correspondant
+    pass
+
+
+@app.route("/Reservation/",methods=["GET","POST"])
+def Reservation():
+    if request.method=="GET": #Renvoie la liste des réservations
         pass
-"""
+    elif request.method=="POST": #permet d'ajouter une Réservation (utilisateur)
+        pass
+
+@app.route("/Reservation/<id>/", methods=["GET"])
+def Reservation_ID(): #renvoie la réservation avec l'ID correspondant
+    pass
+
+@app.route("/RessourceReserve/", methods=["GET"])
+def RessourceReserve(): #retourne la liste de toutes les ressouces réservé de tout les temps(admin?)
+    pass
+
+@app.route("/RessourceReserve/idRessource/<idRessource>", methods=["GET"])
+def RessourceReserve_idRessource(): #retourne la liste des reservations d'une ressouce spécifié
+    pass
+
+@app.route("/RessourceReserve/idReservation/<idReservation>", methods=["GET"])
+def RessourceReserve_idReservation():#retourne la liste des ressource d'une réservation spécifié
+    pass
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port=5000, debug=True)
