a91ae0221524e18c67f6fa5f7252076209638877
remid1 <remi.delattre@edu.devinci.fr>
2021-03-09 17:57:17 +0100

    endpoint fini!

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 
 from sqlalchemy import create_engine,select,insert, between
-
+from sqlalchemy.sql import and_, or_
 from sqlalchemy.orm import sessionmaker
 from config import *
 
@@ -132,7 +132,7 @@ def RessourceReserve(): #retourne la liste de toutes les ressouces réservé de 
             return "invalid json data" #on vérifie que les élément du json existe
         else:
             content=request.json
-            query = select([ressource_reserve]).where(ressources.c.id==content['idRessource'])
+            query = select([ressources]).where(ressources.c.id==content['idRessource'])
             conn = engine.connect()
             res = conn.execute(query)
             ressourceExiste=False
@@ -144,10 +144,10 @@ def RessourceReserve(): #retourne la liste de toutes les ressouces réservé de 
             for row in res:
                 ressourceExiste=True
                 quantiteGPU = row[2]
-                quantiteMemoire= row[3]
+                quantiteMemoire= row[1]
             if content["nbGPU"]<quantiteGPU:
                 globalGPUValid=True
-            if content["nbGPU"]<quantiteMemoire:
+            if content["nbMemoire"]<quantiteMemoire:
                 globalmemoryValid = True
             query = select([reservations]).where(reservations.c.id==content['idReservation'])
             conn = engine.connect()
@@ -164,20 +164,50 @@ def RessourceReserve(): #retourne la liste de toutes les ressouces réservé de 
                     'date_fin':row[3]
                 }
             if globalGPUValid and globalmemoryValid and ressourceExiste and reservationExiste:
-                query = select([ressource_reserve, reservations]).where(reservations.c.id == ressource_reserve.c.idReservation 
-                    and ressource_reserve.c.idReservation== currentReservation["id"] 
-                    and (between(reservations.c.date_debut,currentReservation["date_debut"],currentReservation["date_fin"])
-                        or between(reservations.c.date_fin,currentReservation["date_debut"],currentReservation["date_fin"])
-                        or between(currentReservation["date_debut"],reservations.c.date_debut,reservations.c.date_fin)
+                query = select([ressource_reserve, reservations]).where(
+                    and_(
+                        reservations.c.id == ressource_reserve.c.idReservation,
+                        ressource_reserve.c.idRessource == content['idRessource'],
+                        or_(
+                            between(reservations.c.date_debut,currentReservation["date_debut"],currentReservation["date_fin"]),
+                            between(reservations.c.date_fin,currentReservation["date_debut"],currentReservation["date_fin"]),
+                            between(currentReservation["date_debut"],reservations.c.date_debut,reservations.c.date_fin)
+                        )
                     )
-                )
+                ).order_by('date_debut')
+                conn = engine.connect()
+                res= None
+                res = conn.execute(query)
+                currentReservationRessource=[content["nbGPU"],content["nbMemoire"]]
+
+                tabdatenonfini=[] #on ajoute [nbGPU, nbMemoire, dateFin] 
+                quantiteRessourceutilise=[0,0] # [nbGPU, nbMemoire]
+
+                chaine=""
+                for row1 in res:
+                    for row2 in tabdatenonfini : 
+                        # si date fin est avant datedebut de row1 on soustrait dans quantité ressource utillisé et on remove l'élément de tabdatenonfini
+                        #return str(type(row2[2]))+ "  " +str(type(row1[6])) + " \n" +chaine+ str(content['idRessource']) + str(row2[2]) + str(row1[6])
+                        if row2[2] < row1[6] :
+                            quantiteRessourceutilise[0]-=row2[0]
+                            quantiteRessourceutilise[0]-=row2[0]
+                            tabdatenonfini.remove(row2)
+                    #on ajoute les ressource utilisé par row1 a quantiteRessourceutilise
+                    quantiteRessourceutilise[0] +=row1[2]
+                    quantiteRessourceutilise[1] +=row1[3]
+                    #si quantité ressource utilisé > quantité max on retourne pas possible
+                    if(quantiteRessourceutilise[0]+currentReservationRessource[0]>quantiteGPU or quantiteRessourceutilise[1]+currentReservationRessource[1]>quantiteMemoire):
+                        return "Request impossible dépassement ressource" +"quantitéGPU "+ str(quantiteRessourceutilise[0])+"  " + str(currentReservationRessource[0]) +" "+ str(quantiteGPU)
+                    tabdatenonfini.append([row1[2],row1[3],row1[7]])
+                # insertion de la ressourceReservé
+                content = request.json
+                query= ressource_reserve.insert(None).values(idRessource=content['idRessource'],idReservation=content['idReservation'],nbGPU=content['nbGPU'],nbmemoire=content['nbMemoire'])
                 conn = engine.connect()
                 res = conn.execute(query)
-                tabdate=[currentReservation['date_debut'], currentReservation['date_fin']]
-                tabGPU=[content["nbGPU"]]
-                for row in res:
-                    
-                return str(res)
+                return "200"
+
+                                
+
     
 
 @app.route("/RessourceReserve/idRessource/<int:id>", methods=["GET"])
