925893561fc5254bc4a9c47230d163eaa75733ab
ArthurVarez <71933402+ArthurVarez@users.noreply.github.com>
2021-03-16 08:23:15 +0100

    unit test ..

--- app/test.py
+++ app/test.py
@@ -7,15 +7,13 @@ class TestAPI(unittest.TestCase):
 	
 	def setUp(self):
 		self.app = app.test_client()
-		
-
-	def test_welcomepage(self):
 		response = self.app.get("/")
 		self.assertEqual(response.status_code,200)
-
+		
 	def test_ressource(self):
 		response_ressource = self.app.get("/Ressource/")
 		self.assertEqual(response_ressource.status_code,200)
+		self.assertIsInstance(reponse,json)
 	def test_ressource_post(self):
 		pass
 		
